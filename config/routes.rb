0fcc90ef277cb50b82c94f15febbb210b46f48c4
tavo <gasb_150@hotmail.com>
2021-06-12 11:43:22 -0500

    Create skill, framework, language scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :skills
+  resources :languages
+  resources :frameworks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
