7b0d3b456bf7a548c705bbb9e531feedf1e09715
tavo <gasb_150@hotmail.com>
2021-06-13 21:53:53 -0500

    Change seed and show html

--- db/migrate/20210612170051_create_proyects.rb
+++ db/migrate/20210612170051_create_proyects.rb
@@ -3,6 +3,7 @@
 class CreateProyects < ActiveRecord::Migration[6.1]
   def change
     create_table :proyects do |t|
+      t.integer :priority
       t.string :name
       t.references :framework, null: false, foreign_key: true
       t.references :language, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,28 @@
 # Frameworks
 Framework.create(name: 'NA').save
 Framework.create(name: 'Rails').save
+Framework.create(name: 'React').save
+Framework.create(name: 'Rspec').save
+Framework.create(name: 'Capibara').save
+Framework.create(name: 'Selenium').save
+
+
 
 # Language
 Language.create(name: 'NA').save
 Language.create(name: 'Ruby').save
+Language.create(name: 'HTML').save
+Language.create(name: 'CSS').save
+Language.create(name: 'JavaScript (ES5/ES6)').save
+
 
 # Skill
 Skill.create(name: 'NA').save
 Skill.create(name: 'GitLAb').save
+Skill.create(name: 'Codekit').save
+Skill.create(name: 'Codepen').save
+Skill.create(name: 'GitHub').save
+Skill.create(name: 'Terminal').save
+
+
+
